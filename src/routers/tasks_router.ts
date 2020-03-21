ea01df07398218a330ba8a265e30c2e35ae88c03
Rafael Ferreira <rafaelsferreira@icloud.com>
2020-03-21 17:17:20 -0300

    Small changes

--- src/routers/tasks_router.ts
+++ src/routers/tasks_router.ts
@@ -4,35 +4,35 @@ import { TasksController } from '../controllers/tasks_controller'
 
 const tasksRouter = express.Router()
 
-tasksRouter.get('/', (req, res) => {
+tasksRouter.get('/', (req: express.Request, res: express.Response) => {
     (new TasksController()).findAllTasks()
         .then(result => {
             res.json(result)
         })
 })
 
-tasksRouter.post('/', (req, res) => {
+tasksRouter.post('/', (req: express.Request, res: express.Response) => {
     (new TasksController()).createTask(req.body)
         .then(result => {
             res.status(200).send(result)
         })
 })
 
-tasksRouter.get('/:id', (req, res) => {
+tasksRouter.get('/:id', (req: express.Request, res: express.Response) => {
     (new TasksController()).findTaskById(parseInt(req.params.id))
         .then(result => {
             res.status(200).send(result)
         })
 })
 
-tasksRouter.put('/:id', (req, res) => {
+tasksRouter.put('/:id', (req: express.Request, res: express.Response) => {
     (new TasksController()).updateTask(parseInt(req.params.id), req.body)
         .then(result => {
             res.status(200).send(result)
         })
 })
 
-tasksRouter.delete('/:id', (req, res) => {
+tasksRouter.delete('/:id', (req: express.Request, res: express.Response) => {
     (new TasksController()).removeTask(parseInt(req.params.id))
         .then(result => {
             res.status(200).send(result)
